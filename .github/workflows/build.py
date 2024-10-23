7a94a765c66fa3c6e81824c12c9d956f385b25a3
Hanzla Ansari <57278344+hanzla-spec@users.noreply.github.com>
2024-10-23 22:01:42 +0530

    Update build.py

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -1,23 +1,10 @@
-name: Build and Deploy
+name: Build and Deployss
 
 on:
   push:
     branches:
       - feature
-  workflow_dispatch:
-    inputs:
-      branch:
-        description: 'Branch name'
-        required: false
-      commit:
-        description: 'Commit SHA'
-        required: false
-      author:
-        description: 'Author of the commit'
-        required: false
-      changed_files:
-        description: 'changed files'
-        required: false
+
         
 jobs:
   build_and_deploy:
