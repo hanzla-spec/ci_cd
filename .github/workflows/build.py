57e5be9792eb354abbb09edd412adb6044966d47
Hanzla Ansari <57278344+hanzla-spec@users.noreply.github.com>
2024-10-23 21:58:24 +0530

    Create build.py

---
+++ .github/workflows/build.py
@@ -0,0 +1,28 @@
+name: Build and Deploy
+
+on:
+  workflow_dispatch:
+    inputs:
+      branch:
+        description: 'Branch name'
+        required: false
+      commit:
+        description: 'Commit SHA'
+        required: false
+      author:
+        description: 'Author of the commit'
+        required: false
+      changed_files:
+        description: 'changed files'
+        required: false
+        
+jobs:
+  build_and_deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v2
+          
+      - name: Use metadata from api
+        run: |
+          echo "Branch: ${{ github.event.inputs.changed_files }}"
